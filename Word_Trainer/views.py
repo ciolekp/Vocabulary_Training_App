6e7f82997edb64a10a434508a2083a26208cb599
ciolekp <pciolek.12@gmail.com>
2025-07-17 20:16:45 +0200

    update

--- Word_Trainer/views.py
+++ Word_Trainer/views.py
@@ -5,16 +5,15 @@ from .serializers import WordSerializer
 import random
 from rest_framework import viewsets
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login as auth_login
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            auth_login(request, user)
-            return redirect('learn_english')
+            form.save()
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
